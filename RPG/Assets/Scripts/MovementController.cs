06035d79e337aa4966e79503631fdffec12b88be
dglipp <dg.lippolis94@gmail.com>
2022-04-08 19:01:48 +0200

    added animations prefabs first script

---
+++ RPG/Assets/Scripts/MovementController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+    public float movementSpeed = 3.0f;
+    Vector2 movement = new Vector2();
+    RigidBody2D rb2D;
+
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
